c06bf24e02cacc6720c6295953c610b1ca1c2ef2
LeoDog896 <LeoDog896@hotmail.com>
2022-09-29 20:08:56 +0000

    Pub

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,39 @@
 <script lang="ts">
+  import type { Writable } from "svelte/store"
   import { onMount, onDestroy } from 'svelte'
-  import { Editor } from '@tiptap/core'
+  import { Editor, type JSONContent } from '@tiptap/core'
   import StarterKit from '@tiptap/starter-kit'
   import Placeholder from '@tiptap/extension-placeholder'
+  import { localStore } from 'svelte-persistent';
+  import writableDerived from "svelte-writable-derived";
+
+  let store = localStore("key", "defaultValue")
 
   let element: HTMLDivElement
   let editor: Editor
+  let content: Writable<JSONContent> = writableDerived(
+    localStore("content", "{}"),
+    JSON.parse,
+    JSON.stringify
+  )
+
+  $: {
+    if (editor) $content = editor.getJSON() ?? {}
+  }
 
   onMount(() => {
     editor = new Editor({
       element: element,
       extensions: [
-        StarterKit,
+        StarterKit.configure({
+          bulletList: false,
+          orderedList: false
+        }),
         Placeholder.configure({
-          placeholder: 'go ahead. put something in',
+          placeholder: "Type anything..."
         })
       ],
-      content: '',
+      content: $content,
       onTransaction: () => {
         // force re-render so `editor.isActive` works as expected
         editor = editor
@@ -46,6 +63,7 @@
     outline: none;
     min-height: calc(100vh - 4rem);
     padding: 2rem;
+    font-family: monospace;
   }
 
   :global(p) {
