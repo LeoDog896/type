ac8be94adcd1c65a6a23c77d5f834fa6e70537ad
LeoDog896 <LeoDog896@hotmail.com>
2022-09-29 20:39:16 +0000

    fmt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,74 +1,73 @@
 <script lang="ts">
-  import type { Writable } from "svelte/store"
-  import { onMount, onDestroy } from 'svelte'
-  import { Editor, type JSONContent } from '@tiptap/core'
-  import StarterKit from '@tiptap/starter-kit'
-  import Placeholder from '@tiptap/extension-placeholder'
-  import { localStore } from 'svelte-persistent';
-  import writableDerived from "svelte-writable-derived";
+	import type { Writable } from 'svelte/store';
+	import { onMount, onDestroy } from 'svelte';
+	import { Editor, type JSONContent } from '@tiptap/core';
+	import StarterKit from '@tiptap/starter-kit';
+	import Placeholder from '@tiptap/extension-placeholder';
+	import { localStore } from 'svelte-persistent';
+	import writableDerived from 'svelte-writable-derived';
 
-  let store = localStore("key", "defaultValue")
+	let store = localStore('key', 'defaultValue');
 
-  let element: HTMLDivElement
-  let editor: Editor
-  let content: Writable<JSONContent> = writableDerived(
-    localStore("content", "{}"),
-    JSON.parse,
-    JSON.stringify
-  )
+	let element: HTMLDivElement;
+	let editor: Editor;
+	let content: Writable<JSONContent> = writableDerived(
+		localStore('content', '{}'),
+		JSON.parse,
+		JSON.stringify
+	);
 
-  $: {
-    if (editor) $content = editor.getJSON() ?? {}
-  }
+	$: {
+		if (editor) $content = editor.getJSON() ?? {};
+	}
 
-  onMount(() => {
-    editor = new Editor({
-      element: element,
-      extensions: [
-        StarterKit.configure({
-          bulletList: false,
-          orderedList: false
-        }),
-        Placeholder.configure({
-          placeholder: "Type anything..."
-        })
-      ],
-      content: $content,
-      onTransaction: () => {
-        // force re-render so `editor.isActive` works as expected
-        editor = editor
-      },
-    })
-  })
+	onMount(() => {
+		editor = new Editor({
+			element: element,
+			extensions: [
+				StarterKit.configure({
+					bulletList: false,
+					orderedList: false
+				}),
+				Placeholder.configure({
+					placeholder: 'Type anything...'
+				})
+			],
+			content: $content,
+			onTransaction: () => {
+				// force re-render so `editor.isActive` works as expected
+				editor = editor;
+			}
+		});
+	});
 
-  onDestroy(() => {
-    if (editor) {
-      editor.destroy()
-    }
-  })
+	onDestroy(() => {
+		if (editor) {
+			editor.destroy();
+		}
+	});
 </script>
 
 <div bind:this={element} />
 
 <style>
-  :global(.ProseMirror p.is-editor-empty:first-child::before) {
-    color: #adb5bd;
-    content: attr(data-placeholder);
-    float: left;
-    height: 0;
-    pointer-events: none;
-  }
+	:global(.ProseMirror p.is-editor-empty:first-child::before) {
+		color: #adb5bd;
+		content: attr(data-placeholder);
+		float: left;
+		height: 0;
+		pointer-events: none;
+	}
 
-  :global(.ProseMirror) {
-    outline: none;
-    min-height: calc(100vh - 4rem);
-    padding: 2rem;
-    font-family: monospace;
-  }
+	:global(.ProseMirror) {
+		outline: none;
+		min-height: calc(100vh - 4rem);
+		padding: 2rem;
+		font-family: monospace;
+	}
 
-  :global(p) {
-    margin-top: 0px;
-    margin-bottom: 0px;
-  }
-
+	:global(p) {
+		margin-top: 0px;
+		margin-bottom: 0px;
+	}
 </style>
